af6d582199ee1af71e32ffaff553e41f6763e052
Joe Thornber <joe.thornber@gmail.com>
2011-05-03 10:50:21 +0100

    couple renames (Mike Snitzer)

--- drivers/md/persistent-data/dm-pd-module.c
+++ drivers/md/persistent-data/dm-pd-module.c
@@ -1,17 +1,17 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-static int persistent_data_init(void)
+static int dm_persistent_data_init(void)
 {
 	return 0;
 }
 
-static void persistent_data_exit(void)
+static void dm_persistent_data_exit(void)
 {
 }
 
-module_init(persistent_data_init);
-module_exit(persistent_data_exit);
+module_init(dm_persistent_data_init);
+module_exit(dm_persistent_data_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Joe Thornber");
