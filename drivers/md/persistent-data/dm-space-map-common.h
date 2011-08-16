921fa22b0b03a19710f7c25b4d98b24115674ffd
Joe Thornber <ejt@redhat.com>
2011-08-16 14:17:43 +0100

    [space-map-metadata] move the ll_* routines into a common file, ready to be shared with sm-disk.

--- drivers/md/persistent-data/dm-space-map-common.h
+++ drivers/md/persistent-data/dm-space-map-common.h
@@ -98,6 +98,24 @@ int sm_find_free(void *addr, unsigned begin, unsigned end, unsigned *result);
 
 void *dm_bitmap_data(struct dm_block *b);
 
+/* FIXME: hide this in common ? */
 extern struct dm_block_validator dm_sm_bitmap_validator;
+
+/*----------------------------------------------------------------*/
+
+int sm_ll_new(struct ll_disk *ll, struct dm_transaction_manager *tm,
+	      dm_block_t nr_blocks);
+int sm_ll_open(struct ll_disk *ll, struct dm_transaction_manager *tm,
+	       void *root_le, size_t len);
+int sm_ll_lookup_bitmap(struct ll_disk *ll, dm_block_t b, uint32_t *result);
+int sm_ll_lookup(struct ll_disk *ll, dm_block_t b, uint32_t *result);
+int sm_ll_find_free_block(struct ll_disk *ll, dm_block_t begin,
+			  dm_block_t end, dm_block_t *result);
+int sm_ll_insert(struct ll_disk *ll, dm_block_t b, uint32_t ref_count);
+int sm_ll_inc(struct ll_disk *ll, dm_block_t b);
+int sm_ll_dec(struct ll_disk *ll, dm_block_t b);
+int sm_ll_commit(struct ll_disk *ll);
+
+/*----------------------------------------------------------------*/
 
 #endif	/* DM_SPACE_MAP_COMMON_H */
