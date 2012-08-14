e84e1bd224a02132a9edeb1be16efb20963baaa8
Joe Thornber <ejt@redhat.com>
2012-08-14 12:35:44 +0100

    [dm-cache] allow concurrent migrations

--- drivers/md/dm-cache-target.c
+++ drivers/md/dm-cache-target.c
@@ -652,7 +652,7 @@ static void process_bio(struct cache_c *c, struct bio *bio)
 	struct policy_result lookup_result;
 	struct dm_cache_endio_hook *h = dm_get_mapinfo(bio)->ptr;
 	bool cheap_copy = !test_bit(block, c->dirty_bitset);
-	bool can_migrate = (atomic_read(&c->nr_migrations) == 0) &&
+	bool can_migrate = (atomic_read(&c->nr_migrations) < 4) &&
 		times_below_percentage(&c->migration_times, 100); /* FIXME: hard coded value */
 
 	/*
@@ -699,18 +699,20 @@ static void process_bio(struct cache_c *c, struct bio *bio)
 		build_key(lookup_result.old_oblock, &key);
 		r = bio_detain(c->prison, &key, bio, &old_ocell);
 		if (r > 0) {
-			/* hmm, awkward */
-			pr_alert("demoting a migrating block :( old_oblock = %lu, new_oblock = %lu, cache = %lu, nr_migrating = %lu\n",
-				 (unsigned long) lookup_result.old_oblock,
-				 (unsigned long) block,
-				 (unsigned long) lookup_result.cblock,
-				 (unsigned long) atomic_read(&c->nr_migrations));
-			BUG();
-		} else {
-			writeback_then_promote(c, lookup_result.old_oblock, block,
-					       lookup_result.cblock,
-					       old_ocell, new_ocell);
+			/*
+			 * We have to be careful to avoid lock inversion of
+			 * the cells.  So we back off, and wait for the
+			 * old_ocell to become free.
+			 */
+			policy_force_mapping(c->policy, block,
+					     lookup_result.old_oblock, lookup_result.cblock);
+			pr_alert("cache cell clash, backing off\n");
+			break;
 		}
+
+		writeback_then_promote(c, lookup_result.old_oblock, block,
+				       lookup_result.cblock,
+				       old_ocell, new_ocell);
 		release_cell = 0;
 		break;
 	}
