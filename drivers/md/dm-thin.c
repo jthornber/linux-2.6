a23c962591955344bb49c9fa6bf6acb8a3232e80
Mike Snitzer <snitzer@redhat.com>
2012-08-14 16:51:27 -0400

    dm thin: allow discards even if block size is not a power of 2
    The block layer assumes discard_granularity is a power of 2, so changes
    are needed to allow discards when a pool is using a block size that is
    not a power of 2.  This patch depends on commit c6e666345e1b79c62b
    ("block: split discard into aligned requests").
    
    If discard passdown is enabled: use the data device's
    discard_granularity but make sure block size is a multiple of it.  Also
    make sure the data device's discard_max_sectors is not smaller than the
    pool's block size.
    
    If discard passdown is disabled: use a discard_granularity of
    DATA_DEV_BLOCK_SIZE_MIN_SECTORS if block size isn't a power of 2.  The
    pool's block size is a multiple of DATA_DEV_BLOCK_SIZE_MIN_SECTORS
    (which is a power of 2).
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-thin.c
+++ drivers/md/dm-thin.c
@@ -1886,15 +1886,6 @@ static int pool_ctr(struct dm_target *ti, unsigned argc, char **argv)
 		goto out_flags_changed;
 	}
 
-	/*
-	 * The block layer requires discard_granularity to be a power of 2.
-	 */
-	if (pf.discard_enabled && !is_power_of_2(block_size)) {
-		ti->error = "Discard support must be disabled when the block size is not a power of 2";
-		r = -EINVAL;
-		goto out_flags_changed;
-	}
-
 	pt->pool = pool;
 	pt->ti = ti;
 	pt->metadata_dev = metadata_dev;
@@ -2372,20 +2363,71 @@ static int pool_merge(struct dm_target *ti, struct bvec_merge_data *bvm,
 	return min(max_size, q->merge_bvec_fn(q, bvm, biovec));
 }
 
-static void set_discard_limits(struct pool *pool, struct queue_limits *limits)
+static void set_discard_granularity_no_passdown(struct pool *pool,
+						struct queue_limits *limits)
 {
-	/*
-	 * FIXME: these limits may be incompatible with the pool's data device
-	 */
-	limits->max_discard_sectors = pool->sectors_per_block;
+	limits->discard_granularity = pool->sectors_per_block << SECTOR_SHIFT;
+	if (pool->sectors_per_block_shift < 0) {
+		/*
+		 * Block size is not a power of 2 but the block layer assumes
+		 * discard_granularity is.  Leverage the fact that block size
+		 * is a multiple of DATA_DEV_BLOCK_SIZE_MIN_SECTORS.
+		 */
+		limits->discard_granularity =
+			DATA_DEV_BLOCK_SIZE_MIN_SECTORS << SECTOR_SHIFT;
+	}
+}
+
+static void set_discard_limits(struct pool *pool, struct block_device *src_bdev,
+			       struct queue_limits *limits)
+{
+	struct request_queue *q = bdev_get_queue(src_bdev);
+	struct queue_limits *src_limits = &q->limits;
 
 	/*
-	 * This is just a hint, and not enforced.  We have to cope with
-	 * bios that cover a block partially.  A discard that spans a block
-	 * boundary is not sent to this target.
+	 * We have to cope with discard bios that cover a block partially.
+	 * But a discard that spans a block boundary is not sent to this target.
 	 */
-	limits->discard_granularity = pool->sectors_per_block << SECTOR_SHIFT;
 	limits->discard_zeroes_data = pool->pf.zero_new_blocks;
+	limits->max_discard_sectors = pool->sectors_per_block;
+
+	if (!pool->pf.discard_passdown)
+		goto set_granularity_no_passdown;
+
+	/*
+	 * discard passdown forces the need to establish a
+	 * discard_granularity that will work for both thinp and
+	 * the underlying data device.  So use the data device's
+	 * discard_granularity but make sure block size is a multiple of it.
+	 */
+	limits->discard_granularity = src_limits->discard_granularity;
+
+	if ((limits->max_discard_sectors << SECTOR_SHIFT)
+	    & (limits->discard_granularity - 1)) {
+		DMWARN("%s: max discard (%u) would not be a multiple of discard granularity (%u): Disabling discard passdown.",
+		       dm_device_name(pool->pool_md),
+		       (limits->max_discard_sectors << SECTOR_SHIFT),
+		       limits->discard_granularity);
+		pool->pf.discard_passdown = 0;
+		goto set_granularity_no_passdown;
+	}
+
+	/*
+	 * discard passdown is enabled so we cannot support discards
+	 * that are larger than supported by the underlying data device.
+	 * Conversely, we don't want discards larger than the block size.
+	 */
+	if (src_limits->max_discard_sectors < pool->sectors_per_block) {
+		DMWARN("%s: max discard (%u) would be less than block size (%u): Disabling discard passdown.",
+		       dm_device_name(pool->pool_md),
+		       (src_limits->max_discard_sectors << SECTOR_SHIFT),
+		       pool->sectors_per_block);
+		pool->pf.discard_passdown = 0;
+		/* fall through to call set_discard_granularity_no_passdown */
+	}
+
+set_granularity_no_passdown:
+	set_discard_granularity_no_passdown(pool, limits);
 }
 
 static void pool_io_hints(struct dm_target *ti, struct queue_limits *limits)
@@ -2396,7 +2438,7 @@ static void pool_io_hints(struct dm_target *ti, struct queue_limits *limits)
 	blk_limits_io_min(limits, 0);
 	blk_limits_io_opt(limits, pool->sectors_per_block << SECTOR_SHIFT);
 	if (pool->pf.discard_enabled)
-		set_discard_limits(pool, limits);
+		set_discard_limits(pool, pt->data_dev->bdev, limits);
 }
 
 static struct target_type pool_target = {
@@ -2689,7 +2731,7 @@ static void thin_io_hints(struct dm_target *ti, struct queue_limits *limits)
 
 	blk_limits_io_min(limits, 0);
 	blk_limits_io_opt(limits, pool->sectors_per_block << SECTOR_SHIFT);
-	set_discard_limits(pool, limits);
+	set_discard_limits(pool, tc->pool_dev->bdev, limits);
 }
 
 static struct target_type thin_target = {
