fb2bbbec1589223e0a24c3fa1865146ff80c79ac
mauelsha <heinzm@redhat.com>
2012-11-23 13:47:03 +0100

    dm-cache-policy-{basic,mq}: return value from process_config_option() when we have it

--- drivers/md/dm-cache-policy-mq.c
+++ drivers/md/dm-cache-policy-mq.c
@@ -1087,7 +1087,7 @@ static int process_policy_args(struct mq_policy *mq, int argc, char **argv)
 		int r = process_config_option(mq, mq->threshold_args, argv + u);
 
 		if (r)
-			return -EINVAL;
+			return r;
 	}
 
 	return 0;
