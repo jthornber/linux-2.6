71aa392374a7f2a945ca8d24587a13254822a059
mauelsha <heinzm@redhat.com>
2012-11-29 01:45:14 +0100

    dm-cache-policy-mq: add module alias for default back in

--- drivers/md/dm-cache-policy-mq.c
+++ drivers/md/dm-cache-policy-mq.c
@@ -1217,4 +1217,6 @@ MODULE_AUTHOR("Joe Thornber");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("mq cache policy");
 
+MODULE_ALIAS("dm-cache-default");
+
 /*----------------------------------------------------------------*/
