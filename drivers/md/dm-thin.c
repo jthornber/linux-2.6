b1a565d5ba15b0178ad9d4dd63f8189ebbcb489f
Joe Thornber <ejt@redhat.com>
2014-09-12 11:34:01 +0100

    [dm-thin] Performance improvement to discard processing
    When processing a discard bio, if the block is already quiesced do the
    discard immediately rather than adding the mapping to a list for the
    next iteration of the worker thread.
    
    Discarding a fully provisioned, 100G thin with 64k block size goes
    from 860s to 95s with this change.
    
    Clearly there's something wrong with the worker architecture, more
    investigation needed.

--- drivers/md/dm-thin.c
+++ drivers/md/dm-thin.c
@@ -1184,12 +1184,9 @@ static void process_discard(struct thin_c *tc, struct bio *bio)
 			m->cell2 = cell2;
 			m->bio = bio;
 
-			if (!dm_deferred_set_add_work(pool->all_io_ds, &m->list)) {
-				spin_lock_irqsave(&pool->lock, flags);
-				list_add_tail(&m->list, &pool->prepared_discards);
-				spin_unlock_irqrestore(&pool->lock, flags);
-				wake_worker(pool);
-			}
+			if (!dm_deferred_set_add_work(pool->all_io_ds, &m->list))
+				pool->process_prepared_discard(m);
+
 		} else {
 			inc_all_io_entry(pool, bio);
 			cell_defer_no_holder(tc, cell);
