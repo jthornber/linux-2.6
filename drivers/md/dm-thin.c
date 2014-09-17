f34e2dfceb47359ade4e20fd5242a214870dbeaa
Joe Thornber <ejt@redhat.com>
2014-09-17 10:17:39 +0100

    [dm-bio-prison] introduce support for locking ranges of blocks.
    Ranges will be placed in the same cell if they overlap.

--- drivers/md/dm-thin.c
+++ drivers/md/dm-thin.c
@@ -113,7 +113,8 @@ static void build_data_key(struct dm_thin_device *td,
 {
 	key->virtual = 0;
 	key->dev = dm_thin_dev_id(td);
-	key->block = b;
+	key->block_begin = b;
+	key->block_end = b + 1ULL;
 }
 
 static void build_virtual_key(struct dm_thin_device *td, dm_block_t b,
@@ -121,7 +122,8 @@ static void build_virtual_key(struct dm_thin_device *td, dm_block_t b,
 {
 	key->virtual = 1;
 	key->dev = dm_thin_dev_id(td);
-	key->block = b;
+	key->block_begin = b;
+	key->block_end = b + 1ULL;
 }
 
 /*----------------------------------------------------------------*/
