e06be3ec1acd8a861663ff262995e0f01652b574
Joe Thornber <ejt@redhat.com>
2015-05-15 15:20:09 +0100

    dm-cache: Pass a new 'critical' flag to the policies when requesting writeback work.
    We only allow non critical writeback if the origin is idle.  It is up
    to the policy to decide what writeback work is critical.

--- drivers/md/dm-cache-target.c
+++ drivers/md/dm-cache-target.c
@@ -1778,6 +1778,7 @@ static void writeback_some_dirty_blocks(struct cache *cache)
 	dm_cblock_t cblock;
 	struct prealloc structs;
 	struct dm_bio_prison_cell *old_ocell;
+	bool busy = !iot_idle_for(&cache->origin_tracker, HZ);
 
 	memset(&structs, 0, sizeof(structs));
 
@@ -1785,7 +1786,7 @@ static void writeback_some_dirty_blocks(struct cache *cache)
 		if (prealloc_data_structs(cache, &structs))
 			break;
 
-		r = policy_writeback_work(cache->policy, &oblock, &cblock);
+		r = policy_writeback_work(cache->policy, &oblock, &cblock, busy);
 		if (r)
 			break;
 
