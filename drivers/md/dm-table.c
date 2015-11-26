568b34a6481c55d789a7eecdb681a773c7c6e4f7
Joe Thornber <ejt@redhat.com>
2015-11-26 12:01:26 +0000

    [dm-fake-discard] Allow fake discard to be used to _un_ support discard
    fold into the fake-discard patch

--- drivers/md/dm-table.c
+++ drivers/md/dm-table.c
@@ -1561,6 +1561,9 @@ static bool dm_table_supports_discards(struct dm_table *t)
 		if (!ti->num_discard_bios)
 			continue;
 
+		if (ti->discards_unsupported)
+			return false;
+
 		if (ti->discards_supported)
 			return true;
 
