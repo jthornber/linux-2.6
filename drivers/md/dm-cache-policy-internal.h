98a8a6e4923530c83142224348093071e35ea126
Mike Snitzer <snitzer@redhat.com>
2017-03-02 16:32:21 -0500

    dm cache policy: tweak some docs in dm-cache-policy.h
    Not seeing why we'd ever want set_dirty and clear_dirty to be optional;
    if a policy isn't tracking clean vs dirty then how is it working?
    
    Conflicts:
    	drivers/md/dm-cache-policy.h

--- drivers/md/dm-cache-policy-internal.h
+++ drivers/md/dm-cache-policy-internal.h
@@ -48,14 +48,12 @@ static inline void policy_complete_background_work(struct dm_cache_policy *p,
 
 static inline void policy_set_dirty(struct dm_cache_policy *p, dm_cblock_t cblock)
 {
-	if (p->set_dirty)
-		p->set_dirty(p, cblock);
+	p->set_dirty(p, cblock);
 }
 
 static inline void policy_clear_dirty(struct dm_cache_policy *p, dm_cblock_t cblock)
 {
-	if (p->clear_dirty)
-		p->clear_dirty(p, cblock);
+	p->clear_dirty(p, cblock);
 }
 
 static inline int policy_load_mapping(struct dm_cache_policy *p,
